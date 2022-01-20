e3982023383145943d062860781ceab0462c4cd5
Andrii Mogylnytskyi <88145575+FossaX7@users.noreply.github.com>
2022-01-20 18:53:39 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
 
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+};
+const PROMPT_DELAY =1000;
+
+let timerId = null;
+
+refs.start.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, PROMPT_DELAY);
+    refs.start.setAttribute('disabled', 'true');
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  refs.stop.addEventListener('click', () => {
+      clearInterval(timerId);
+      refs.start.removeAttribute('disabled');
+  });
